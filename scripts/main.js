f5e2d51c067d077579351dbd963107f27e4c40cf
david <zein207@gmail.com>
2021-04-04 19:21:37 -0500

    desktop version

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+//############################SHOW MENU########################################
+
 const navbar = document.getElementById('navbar');
 const menuIcon = document.getElementById('menuIcon');
 const closeIcon = document.getElementById('closeIcon');
@@ -19,15 +21,39 @@ closeIcon.addEventListener('click', showMenu);
 
 mask.addEventListener('click', showMenu);
 
-//####################################################################
+//############################BANNER ANIMATION########################################
 
 const bannerText = document.getElementById('bannerText');
+const bannerImg = document.getElementById('bannerImg');
+
+const bannerText3 = document.getElementById('bannerText3');
+const bannerImg3 = document.getElementById('bannerImg3');
 
 window.addEventListener('load', () => {
+
     bannerText.classList.toggle('show-banner-text');
+    bannerText.style.top = '190px';
 })
 
-//####################################################################
+window.addEventListener('scroll', () => {
+
+    let value = window.scrollY + 380;
+    let valueBanner = window.scrollY - 1690;
+
+    bannerText.style.top = `${value * 0.5}px`;
+    bannerImg.style.top = `${window.scrollY * 0.4}px`;
+
+    if(window.innerWidth > 1024) {
+        bannerText3.style.top = `${valueBanner * 0.6}px`;
+        bannerImg3.style.top = `${valueBanner * 0.4}px`;
+       // console.log(window.scrollY)
+    } else {
+        bannerText3.style.top = '0';
+        bannerImg3.style.top = '0';
+    }
+})
+
+//############################MENU ANIMATION########################################
 
 
 let ubicacion = window.pageYOffset;
@@ -36,25 +62,34 @@ const menuAnimation = () => {
 
     let menu = document.getElementById('menu');
 
-//    console.log(window.pageYOffset);
+    if (window.innerWidth < 1024) {
 
-    let currentScroll = window.pageYOffset;
-    if(ubicacion >= currentScroll) {
-        menu.style.top = '0';
+        //    console.log(window.pageYOffset);
+        
+            let currentScroll = window.pageYOffset;
+            if(ubicacion >= currentScroll) {
+                menu.style.top = '0';
+            } else {
+                menu.style.top = '-70px';
+            }
+            ubicacion = currentScroll;
+        
+            if (window.pageYOffset <= 70) {
+                menu.style.top = '0';
+        //        console.log('something')
+            }
+
     } else {
-        menu.style.top = '-70px';
-    }
-    ubicacion = currentScroll;
 
-    if (window.pageYOffset <= 70) {
         menu.style.top = '0';
-//        console.log('something')
+
     }
+
 }
 
 window.addEventListener('scroll', menuAnimation)
 
-//####################################################################
+//############################SHOW PRODUCT LIST########################################
 
 const productList = document.getElementById('productList');
 const showProductList = document.getElementById('showProductList');
@@ -63,3 +98,7 @@ showProductList.addEventListener('click', () => {
     productList.classList.toggle('show-product-list');
     showProductList.classList.toggle('rotate');
 })
+
+
+//####################################################################
+
